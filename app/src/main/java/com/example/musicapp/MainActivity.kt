5aea2f36201d173a7c3c0d9a9e8a0dd3d7ca6317
MarkelDiez <markelm.diezm@gmail.com>
2023-09-21 15:48:47 +0200

    Cambios de funcionalidad en la barra de volumen

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.musicapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.SeekBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +21,13 @@ class MainActivity : AppCompatActivity() {
                 play.setImageResource(android.R.drawable.ic_media_play)
                 playing = true
             }
+        }
+
+        findViewById<ImageView>(R.id.imgMute).setOnClickListener {
+            findViewById<SeekBar>(R.id.barVolume).setProgress(0)
+        }
+        findViewById<ImageView>(R.id.imgMaxVol).setOnClickListener {
+            findViewById<SeekBar>(R.id.barVolume).setProgress(100)
         }
     }
 }
